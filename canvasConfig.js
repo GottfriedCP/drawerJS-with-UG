78e6061064d7e3a53590d18408c1ca72fcb14ba2
GP <gottfriedcpn@gmail.com>
2021-07-13 10:04:38 +0700

    commit pertama

---
+++ canvasConfig.js
@@ -0,0 +1,45 @@
+var drawerPlugins = [
+    // Drawing tools
+    'Pencil',
+    'Eraser',
+    'Text',
+    'Line',
+    'ArrowOneSide',
+    'ArrowTwoSide',
+    'Triangle',
+    'Rectangle',
+    'Circle',
+    'Image',
+    'BackgroundImage',
+    'Polygon',
+
+    // Drawing options
+    'ColorHtml5',
+    'Color',
+    'ShapeBorder',
+    'BrushSize',
+    'OpacityOption',
+    'LineWidth',
+    'StrokeWidth',
+
+    'Resize',
+    'ShapeContextMenu',
+    'CloseButton',
+    'OvercanvasPopup',
+    'OpenPopupButton',
+    'MinimizeButton',
+    'ToggleVisibilityButton',
+    'MovableFloatingMode',
+    'FullscreenModeButton',
+
+    'TextLineHeight',
+    'TextAlign',
+
+    'TextFontFamily',
+    'TextFontSize',
+    'TextFontWeight',
+    'TextFontStyle',
+    'TextDecoration',
+    'TextColor',
+    'TextBackgroundColor'
+];
